83ae87d2a9e3a3a19ae5f8357e9865844b3c7b97
Lai Xuancheng <lai32690@gmail.com>
2020-01-23 19:21:25 -0300

    creating sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
+import './global.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <div id="app">
+            <aside>
+                <strong>Cadastrar</strong>
+                <form>
+                    <div className="input-block">
+                        <label htmlFor="username_github">Usuúario do Github</label>
+                        <input id="username_github" type="text" name="username_github" required />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="techs">Tecnologias</label>
+                        <input id="techs" type="text" name="techs" required />
+                    </div>
+
+                    <div className="input-group">
+                        <div className="input-block">
+                            <label htmlFor="latitude">Latitude</label>
+                            <input id="latitude" type="text" name="latitude" required />
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="longitude">Longitude</label>
+                            <input id="longitude" type="text" name="longitude" required />
+                        </div>
+                    </div>
+
+                    <button type="submit">Salvar</button>
+                </form>
+            </aside>
+            <main></main>
+        </div>
+        </>
+    );
 }
 
 export default App;
